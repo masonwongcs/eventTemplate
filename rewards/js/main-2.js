8f559d9d0f6dc36b5023e10625c4d9a27be5863e
masonwong <Seong6232@gmail.com>
2018-03-29 00:22:33 +0800

    Styled award 2

--- rewards/js/main-2.js
+++ rewards/js/main-2.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	new QRCode(document.getElementById("qr"), {text: "1234567", width: 180, height: 180});
+    var animation = TweenMax.to($('.confetti'), 1, { ease: Elastic.easeOut.config(1, 0.3), transform: 'scale(1.5)' });
+    // $('.confetti').fadeOut();
+    animation.play()
+    TweenMax.from($('.reward-wrapper'), 0.8, { ease: Elastic.easeOut.config(1, 0.3), transform: 'translateY(-50vh)' });
+    TweenMax.fromTo($('.success-icon'), 2, { ease: Elastic.easeOut.config(1, 0.3), transform: 'rotateX(-90deg)' }, { ease: Elastic.easeOut.config(1, 0.3), transform: 'rotateY(0deg)' });
+    setTimeout(function(){
+        animation.reverse()
+    }, 2000);
 });
--- rewards/js/main.js
+++ rewards/js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-      TweenMax.to($('.confetti'), 1, { ease: Elastic.easeOut.config(1, 0.3), transform: 'scale(1)' });
+      TweenMax.to($('.confetti'), 1, { ease: Elastic.easeOut.config(1, 0.3), transform: 'scale(1.5)' });
+      TweenMax.fromTo($('.success-icon'), 1, { ease: Elastic.easeOut.config(1, 0.3), transform: 'rotateY(-90deg)' }, { ease: Elastic.easeOut.config(1, 0.3), transform: 'rotateY(0deg)' });
+      setTimeout(function(){
+          $('.confetti').fadeOut();
+      }, 2000);
 });
