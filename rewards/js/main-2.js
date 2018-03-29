220f06b1d01ad45afc24b5d545b662285974784d
Mason Wong <Seong6232@gmail.com>
2018-03-29 22:58:00 +0800

    Added new process for rewards

--- rewards/js/main-2.js
+++ rewards/js/main-2.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
-    var animation = TweenMax.to($('.confetti'), 1, { ease: Elastic.easeOut.config(1, 0.3), transform: 'scale(1.5)' });
-    // $('.confetti').fadeOut();
-    animation.play()
-    TweenMax.from($('.reward-wrapper'), 0.8, { ease: Elastic.easeOut.config(1, 0.3), transform: 'translateY(-50vh)' });
-    TweenMax.fromTo($('.success-icon'), 2, { ease: Elastic.easeOut.config(1, 0.3), transform: 'rotateX(-90deg)' }, { ease: Elastic.easeOut.config(1, 0.3), transform: 'rotateY(0deg)' });
+	$(".container .redeem-btn").click(function(){
+		showPopup();
+	});
+
+	$(".reward-wrapper .btn").click(function(){
+		hidePopup();
+	});
+});
+
+function showPopup(){
+	$('.reward-wrapper').fadeIn();
+	var animation = TweenMax.to($('.confetti'), 1, { ease: Elastic.easeOut.config(1, 0.3), transform: 'scale(1.5)' });
+    var reward = TweenMax.from($('.reward-wrapper'), 0.8, { ease: Elastic.easeOut.config(1, 0.3), transform: 'translateY(-50vh)' });
+    var successIcon = TweenMax.fromTo($('.success-icon'), 2, { ease: Elastic.easeOut.config(1, 0.3), transform: 'rotateX(-90deg)' }, { ease: Elastic.easeOut.config(1, 0.3), transform: 'rotateY(0deg)' });
+    animation.play();
+    reward.play();
+    successIcon.play();
     setTimeout(function(){
         animation.reverse()
     }, 2000);
-});
+}
+
+function hidePopup(){
+    $('.reward-wrapper').fadeOut();
+}
